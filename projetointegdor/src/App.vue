2b800d0cf1ab151b5db02cf133b82e89c0b36974
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-05-23 04:06:19 -0300

    Add files via upload

---
+++ projetointegdor/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import HomePage from "./components/HomePage.vue";
+export default {
+  components : { HomePage}
+}
+</script>
+
+<template>
+  <HomePage />
+</template>
+
+<style></style>
